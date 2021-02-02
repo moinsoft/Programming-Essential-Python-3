a56d172ac2ddf40980733a1a4ef14d1bb4917f0b
moinsoft <mmoin.official@gmail.com>
2021-02-02 08:46:56 +0630

    Exercise 3

---
+++ exercise_3.py
@@ -0,0 +1,80 @@
+# Addition program
+
+number1 = input("Please type an integer and press enter: ")
+number2 = input("Please type another integer and press enter: ")
+
+number1 = int(number1)
+number2 = int(number2)
+
+print(number1, "+", number2, "=", number1+number2)
+
+# Arithmetic operators program
+
+print(number1, "-", number2, "=", number1-number2)
+print(number1, "*", number2, "=", number1*number2)
+print(number1, "**", number2, "=", number1**number2)
+print(number1, "/", number2, "=", number1/number2)
+print(number1, "//", number2, "=", number1//number2)
+print(number1, "%", number2, "=", number1 % number2)
+
+
+# Subtraction program
+
+number3 = input("Please type an integer and press enter: ")
+number4 = input("Please type another integer and press enter: ")
+
+number3 = int(number3)
+number4 = int(number4)
+
+print(number3, "-", number4, "=", number3-number4)
+
+# Multiplication program
+
+number5 = input("Please type an integer and press enter: ")
+number6 = input("Please type another integer and press enter: ")
+
+number5 = int(number5)
+number6 = int(number6)
+
+print(number5, "*", number6, "=", number5*number6)
+
+# Exponentiation program
+
+number11 = input("Please type an integer and press enter: ")
+number12 = input("Please type another integer and press enter: ")
+
+number11 = int(number11)
+number12 = int(number12)
+
+print(number11, "**", number12, "=", number11**number12)
+
+
+# Division program
+
+number7 = input("Please type an integer and press enter: ")
+number8 = input("Please type another integer and press enter: ")
+
+number7 = int(number7)
+number8 = int(number8)
+
+print(number7, "/", number8, "=", number7/number8)
+
+# Floor division program
+
+number13 = input("Please type an integer and press enter: ")
+number14 = input("Please type another integer and press enter: ")
+
+number13 = int(number13)
+number14 = int(number14)
+
+print(number13, "//", number14, "=", number13//number14)
+
+# Modulus program
+
+number9 = input("Please type an integer and press enter: ")
+number10 = input("Please type another integer and press enter: ")
+
+number9 = int(number9)
+number10 = int(number10)
+
+print(number9, "%", number10, "=", number9 % number10)
