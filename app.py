4ad55d78c450a54e8752f8f00da7cad78ad3b289
moinsoft <mmoin.official@gmail.com>
2021-02-02 07:21:42 +0630

    Hello World

---
+++ app.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+text_to_print = "Bangladesh"
+
+print(text_to_print)
+
+name = "Mohammad Moin Uddin"
+phone = "01571120010"
+country = "Bangladesh"
+nationality = "Bangladeshi"
+language = "Python"
+
+print("My name is", name)
+print("This is my phone number", phone)
+print(country, "is my mother land. I am proud to be a", nationality)
+print("I am a", language, "programmer.")
