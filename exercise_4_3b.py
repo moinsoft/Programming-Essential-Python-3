2db4b87d0223d985a56cc787db49dda6e6d0c1f6
moinsoft <mmoin.official@gmail.com>
2021-02-06 04:55:10 +0630

    LeapYear solved system two

---
+++ exercise_4_3b.py
@@ -0,0 +1,10 @@
+year = int(input("Please input integer year = "))
+
+if year % 400 == 0:
+  print(year,"is a leap year.")
+elif year % 100 == 0:
+  print(year,"is not a leap year.")
+elif year % 4 == 0:
+  print(year,"is a leap year.")
+else:
+  print(year,"is not a leap year.")
